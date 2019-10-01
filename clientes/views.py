aace7fddb2fd2ccddac585c22a65e9eb7a45582a
Elvis <dbaelvis@gmail.com>
2019-10-01 00:47:16 -0300

    na aula de crud listar clientes 2

--- clientes/views.py
+++ clientes/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Person
+from .forms import PersonForm
+
 
 def persons_list(request):
      persons = Person.objects.all()
      return render(request, 'person.html', {'persons': persons})
 
 def persons_new(request):
-     pass
+     form = PersonForm(request.POST or None, request.FILES or None)
+
+     if form.is_valid():
+          form.save()
+          return redirect('person_list')
+     return render(request, 'person_form.html', {'form': form})
+
 
 # @login_required
 # def persons_list(request):
